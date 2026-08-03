41d83a8794b0789de8fafaaa5002a7f444225515
Paul Amato <lilpauly12@icloud.com>
2026-08-03 10:35:01 -0400

    Added saving and loading running data

--- run_tracker.py
+++ run_tracker.py
@@ -1,43 +1,63 @@
-print("Welcome to Running Tracker!")
+import json
 
-runs = []
+def load_runs():
+    try:
+        with open("runlog.json", "r") as file:
+            runs = json.load(file)
+            return runs
+    except FileNotFoundError:
+        return []
+
+runs = load_runs()
+
+print("Welcome to Running Tracker!")
 
 def runtrack():
     distance = float(input("Enter the distance ran (miles): "))
     time = float(input("Enter the time taken (minutes): "))
     notes = input("Notes? (Press Enter to skip): ")
+
     mins = time / distance
     sec = mins * 60
     minutes, seconds = divmod(sec, 60)
 
-    return distance, time, notes, sec, minutes, seconds
-
-def printlog(distance, time, notes, sec, minutes, seconds):
-
     pace_str = f"{int(minutes)}:{int(seconds):02d}"
 
-    print(f"{distance} miles in {time} minutes logged! Notes: {notes}")
-    print(f"You're average pace for this run was {pace_str}/mile.")
-    runs.append({
+    run = {
         "distance (miles)": distance,
         "time (minutes)": time,
         "notes": notes,
-        "pace (min/mile)": pace_str
-    })
+        "pace (min/mile)": pace_str,
+    }
+
+    return run
+
+def save_runs(runs):
+    with open("runlog.json", "w") as file:
+        json.dump(runs, file, indent=2, ensure_ascii=False)
+
 
 while True:
-    distance, time, notes, sec, minutes, seconds = runtrack()
+    run = runtrack()
 
-    printlog(distance, time, notes, sec, minutes, seconds)
+    runs.append(run)
+
+    print(f"{run['distance (miles)']} miles in {run['time (minutes)']} minutes logged! Notes: {run['notes']}")
+    print(f"You're average pace for this run was {run['pace (min/mile)']}/mile.")
+    
+    save_runs(runs)
 
     again = input("Would you like to log another run? (y/n): ")
     if again != 'y':
         break
 
-display_log = input("Display running log? (y/n): ")
-if display_log == 'y':
-    for run in runs:
-        print(run)
+def show_runs():
+    display_log = input("Display running log? (y/n): ")
+    if display_log == 'y':
+        for run in runs:
+            print(run)
+
+show_runs()
 
 def show_stats():
     statlog = input("Would you like to see your running stats? (y/n): ")
